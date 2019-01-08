15ad96df44340aad3d43863827b8440b8a375b9a
mariyast1 <mariya_dimova_stoikova@abv.bg>
2019-01-08 12:15:24 +0200

    Adding CategoryMenu.cs

---
+++ src/Web/ShoppingApp.Web/Components/CategoryMenu.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Web.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.Categories.OrderBy(p => p.Name);
+            return View(categories);
+        }
+    }
+}
